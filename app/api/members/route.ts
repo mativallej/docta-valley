76ab406e9c787c6f43e495a20cc6247e0b3ab6f0
lucasnasich <58375667+lucasnasich@users.noreply.github.com>
2025-03-12 11:45:40 -0300

    Update route.ts

--- app/api/members/route.ts
+++ app/api/members/route.ts
@@ -52,11 +52,36 @@ const members: Member[] = [
     },
   },
   {
-    id: '5',
+    id: "5",
+    name: "Luciano Joaquín Alfonso",
+    role: "founder",
+    bio: "CEO & Founder at Jobly | Córdoba, Argentina",
+    image_url:
+      "https://media.licdn.com/dms/image/v2/D4D03AQEbYXpYftHB2Q/profile-displayphoto-shrink_400_400/B4DZU9V2QpHYAk-/0/1740490893049?e=1747267200&v=beta&t=qA0Avf2XxbquO3gMN84lWr6i0_dKvF7vvpg1CTstSgs",
+    links: {
+      linkedin:
+        "https://www.linkedin.com/in/ljalfonso?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+  },
+  {
+    id: "6",
+    name: "Matias Sidler",
+    role: "founder",
+    bio: "CTO & Co-Founder at Chatsell | AI Enthusiast",
+    image_url:
+      "https://drive.google.com/file/d/19wuSegXMYxgKgM2sNyD3dg7CD6817UUg/view?usp=sharing",
+    links: {
+      linkedin: "https://linkedin.com/in/matias-sidler",
+      github: "https://github.com/matisidler",
+      twitter: "https://x.com/tias0x",
+    },
+  },
+  {
+    id: '7',
     name: 'Carla Palmieri',
     role: 'member',
     bio: '🚀 CTO & Co-Founder at Curval | 🌎 Córdoba, Argentina',
-    image_url: 'https://pbs.twimg.com/profile_images/1762579024558034944/pALCXx8V_400x400.jpg',
+    image_url: 'https://media.licdn.com/dms/image/v2/D4D03AQG5Yr50NOunEw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1690337728846?e=1747267200&v=beta&t=COrLhm1CfbN3ifp7OzhE7-6OuU-MU4NoaEN7NMkPAmY',
     links: {
       twitter: 'https://twitter.com/Carly23p',
       linkedin: 'https://linkedin.com/in/carla-palmieri',
@@ -64,7 +89,20 @@ const members: Member[] = [
     },
   },
   {
-    id: '6',
+    id: "8",
+    name: "Ivan Hadrowa",
+    role: "founder",
+    bio: "CEO & Co-Founder at Chatsell | Córdoba, Argentina",
+    image_url:
+      "https://drive.google.com/file/d/1lixy_EwBBkFxuy6lSbeaN0ib1cu1bPw8/view?usp=sharing",
+    links: {
+      linkedin: "https://www.linkedin.com/in/ivanhadrowa/",
+      website: "https://chatsell.net",
+      instagram: "https://www.instagram.com/ivanhadrowa",
+    },
+  },
+  {
+    id: '9',
     name: 'Luciano Juarez',
     role: 'member',
     bio: '🤖 Founder at Botix | 🌎 Córdoba, Argentina',
@@ -74,6 +112,18 @@ const members: Member[] = [
       linkedin: 'https://www.linkedin.com/in/juarez-luciano/'
     },
   },
+  {
+    id: "10",
+    name: "Juan Rios",
+    role: "member",
+    bio: "Software Engineer - COO Phorus Group",
+    image_url:
+      "https://media.licdn.com/dms/image/v2/D4D03AQFN8uzhCmoTFg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1675778796326?e=1747267200&v=beta&t=qQZiSoWuhZQcc86PuyKHhYqAHl4PH9FN-X9ga5xgXUk",
+    links: {
+      linkedin: "https://www.linkedin.com/in/jir125/",
+      twitter: "https://x.com/jirios125",
+    },
+  }
 ];
 
 
